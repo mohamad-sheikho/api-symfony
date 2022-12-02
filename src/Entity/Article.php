b167cabaa111f34ac7c593f6c66f7fd24dc5ff0b
dark sassuke <mohamad.sheikho@laplateforme.io>
2022-12-02 16:29:13 +0100

    30%
    installation api platform
    j'ai commencé la sérialization et les groups

---
+++ src/Entity/Article.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
+use DateTimeImmutable;
+use Symfony\Component\Serializer\Annotation\Groups;
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ApiResource
+(
+    normalizationContext: ['groups' => ['read']], 
+    denormalizationContext:['groups' => ['write:Post']],
+    operations:[
+        new Put(),
+        new Delete(),
+        new Get(
+            normalizationContext: [
+                'groups' => ['read:collection', 'read:item', 'read:Post']
+            ]
+     
+        ),
+        new GetCollection(),
+        new Post(
+            normalizationContext: [
+                'groups' => ['read:collection', 'read:item', 'read:Post']
+            ]
+        ),
+
+        
+    ]
+    
+)]
+
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['read:collection'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['read:collection', 'write:Post'])]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['read:collection', 'write:Post'])]
+    private ?string $slug = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['read:item', 'write:Post'])]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['read:item'])]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[Groups(['read:item', 'write:Post'])]
+    private ?Category $category = null;
+
+
+    public function __construct(){
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = new DateTimeImmutable();
+    }
+   
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
